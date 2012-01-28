0bf21aae576fc3c1acfcb935d08386560c291f8c
Nieve <nievegoor@gmail.com>
2012-01-28 01:50:20 +0000

    refactored NewTypeFileFormatResolver to use OCP

--- Src/Refactoring/GenerateNewType/NewTypeFileFormatResolver.cs
+++ Src/Refactoring/GenerateNewType/NewTypeFileFormatResolver.cs
@@ -8,17 +8,31 @@ namespace MonoDevelop.Stereo.Refactoring.GenerateNewType
 		string ResolveFileFormat(string typeName);
 	}
 	
+	internal delegate string FileFormatResolving(string typeName);
+	
 	public class NewTypeFileFormatResolver : IResolveNewTypeFileFormat
 	{
+		static FileFormatResolving interfaceFormat = name => {
+			if (name.Length > 1 && name[0] == 'I' && char.GetUnicodeCategory(name[1]) == UnicodeCategory.UppercaseLetter)
+				return "namespace {0} {{\r\n\tpublic interface {1}{{\r\n\t\t\r\n\t}}\r\n}}";
+				return null;
+		};
+		
+		static FileFormatResolving classFormat = name => "namespace {0} {{\r\n\tpublic class {1}{{\r\n\t\t\r\n\t}}\r\n}}";
+		
+		FileFormatResolving[] resolvings = new []{interfaceFormat, classFormat};
+		
 		public string ResolveFileFormat (string typeName)
 		{
-			if (typeName.Length > 1 &&
-			    typeName[0] == 'I' && char.GetUnicodeCategory(typeName[1]) == UnicodeCategory.UppercaseLetter) {
-				return "namespace {0} {{\r\n\tpublic interface {1}{{\r\n\t\t\r\n\t}}\r\n}}";
+			if (typeName == null) throw new ArgumentNullException("typeName", "new type name cannot be null");
+			string format = null;
+			foreach (var resolvingAttempt in resolvings) {
+				format = resolvingAttempt(typeName);
+				if (format != null) return format;
 			}
 			
-			return "namespace {0} {{\r\n\tpublic class {1}{{\r\n\t\t\r\n\t}}\r\n}}";
+			throw new ArgumentException("typeName", "{0} cannot be used as a new type name".ToFormat(typeName));
 		}
-	}
+	}	
 }
 
